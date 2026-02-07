970babc3c5ce6c117c080e9be73d517c46299580
Kedars7 <146555458+Kedars7@users.noreply.github.com>
2026-02-07 11:32:58 +0530

    changes in admin

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 function Sidebar() {
     return (
         <div className='bg-[#003a10] min-h-screen pl-[4vw]'>
-            <img src={assets.logo} className='mt-5 w-[max(10vw,100px)] hidden sm:block' alt="logo" />
-            <img src={assets.logo_small} className='mt-5 w-[max(10vw,10px)] mr-5 block sm:hidden' alt="logo_small" />
+            <img src={assets.vybe_logo} className='mt-5 w-[max(10vw,100px)] hidden sm:block' alt="logo" />
+            <img src={assets.vybe_logo} className='mt-5 w-[max(10vw,10px)] mr-5 block sm:hidden' alt="logo_small" />
             <div className="flex flex-col gap-5 mt-10">
                 <NavLink to={"/add-song"} className="flex items-center gap-2.5 text-gray-800 bg-white border border-black p-2 pr-[max(8vw,10px)] drop-shadow-[-4px_4px_#00ff5b] text-sm font-medium">
                     <img src={assets.add_song} className='w-5' alt="Add Song" />
