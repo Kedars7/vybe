2c06e4152e5c9d2dfe6dd47b10944afbca00d458
Kedars7 <146555458+Kedars7@users.noreply.github.com>
2026-02-07 13:19:10 +0530

    added frontend url

--- backend/server.js
+++ backend/server.js
@@ -70,8 +70,9 @@ io.on("connection", (socket) => {
 //middlewares
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://vybe-bice.vercel.app',
   credentials: true
 }))
 
